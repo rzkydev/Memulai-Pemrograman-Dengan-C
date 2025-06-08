0cbd6354b1aeffc71004854ca3f4ea837715b9b2
RZKY <rzky.dev.id@gmail.com>
2025-06-08 19:51:37 +0700

    3rd commit

---
+++ initvar.c
@@ -0,0 +1,22 @@
+/* File : initvar.c */
+/* Penulis : Muhammad Rizky Aulia Ramadhan */
+/* Mengisi variabel i yg bertype in dengan nilai 5 dan menuliskannya */
+
+#include <stdio.h>
+
+int main()
+{ /* Kamus */
+
+    int bebek= 10; /* bebekku ada 10 */
+    float luas= 2.5; /* luas kebunkan 2.5 hektar */
+    float keliling= 2 * 3.14 * 5.5; /* keliling dari sebuah lingkaran yang jari-jarinya 5.5. cm */
+
+    int nilaiku= 3 * 5; 
+
+    int j;
+
+    int i = 5; /* deklarasi dan inisialisasi nilai variabel i dengan 5 */
+    /* Algoritma */
+    printf ("Ini nilai i : %d \n", i); /* untuk mengamati dampak inisialisasi */
+    return 0;
+}
