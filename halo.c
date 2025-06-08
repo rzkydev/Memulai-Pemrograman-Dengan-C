223c33ed624e044eedeac449f5f8e6749ae9480e
RZKY <rzky.dev.id@gmail.com>
2025-06-08 19:16:47 +0700

    first commit

---
+++ halo.c
@@ -0,0 +1,28 @@
+/* File halo.c */
+/* Penulis: Muhammad Rizky Aulia Ramadhan */
+/* Program menuliskan "Halo Dunia!" dan "end-of-line" (ganti baris) */
+
+#include <stdio.h>
+
+int 
+main(void)
+{   /* Badan Program */
+    printf("Halo Dunia!\n");
+    printf("Halo Dunia!\n");
+    printf("Halo Dunia!\n\n");
+
+    printf("Menggapai Impian\n");
+    printf("Senyum terukir tipis\n");
+    printf("Menghias bibir yang manis\n");
+    printf("Langkah demi langkah berpijak\n");
+    printf("Mengejar angan yang bijak\n\n");
+    printf("Sejuta harapan kurengkuh\n");
+    printf("Laksa rintangan kutempuh\n");
+    printf("Laksa menuju kemenangan\n");
+    printf("Menggapai impian\n\n");
+    printf("Riang gembira jalan hidup\n");
+    printf("Hati ikhlas bahagia datang\n");
+    printf("Perjuangan dan doa penuh ikhlas\n");
+    printf("Bawa berkah yang berlimpah\n");
+    return 0;
+}
