3baffe6abd192c612db4c8bfbea75e641d57444f
RZKY <rzky.dev.id@gmail.com>
2025-06-09 07:24:08 +0700

    5th commit

---
+++ assignf.c
@@ -0,0 +1,17 @@
+/* File : assignf.c */
+/* Penulis : Muhammad Rizky Aulia Ramadhan */
+/* Mengisi variabel i yg bertype in dengan nilai 5 dan menuliskannya */
+
+#include <stdio.h>
+
+int main()
+{   /* Kamus */
+    float f = 0.555;
+    float x = 1.5E3;
+
+    /* Algoritma */
+    printf ("Ini nilai f : %f \n", f);
+    printf ("Ini nilai f : %5.2f \n", f);
+    printf ("Ini nilai x : %10.2f \n", x);
+    return 0;
+}
