d2a8b36f3dd0aa458143ff1759bb4298fdb668e9
R A U L <rzky.dev.id@gmail.com>
2025-06-11 07:47:00 +0700

    10th commit

---
+++ aritmatika.c
@@ -0,0 +1,17 @@
+/* File : aritmatika.c */
+/* Penulis : Muhammad Rizky Aulia Ramadhan */
+/* Operasi aritmatika bilangan integer dan bilangan riil :
+    Menuliskan jumlah, hasil perkalian, pembagian, modulo
+    dua buah variabel integer */
+
+#include <stdio.h>
+
+int main () {
+    /* Kamus */
+    int x = 5;
+    int y = 8;
+
+    /* Program */
+    printf("Ini nilai x + y : %d\n", x + y);
+    printf("Ini nilai x - y : %d\n", x - y);
+}
