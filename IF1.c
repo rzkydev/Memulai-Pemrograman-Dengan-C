8c1101c1d8bc54513b8248acd0fc7ab15c868c47
R A U L <rzky.dev.id@gmail.com>
2025-06-11 10:57:53 +0700

    15th commit

---
+++ IF1.c
@@ -0,0 +1,25 @@
+/* File: IF1.c */
+/* Penulis : Muhammad Rizky Aulia Ramadhan */
+/* Deskripsi : */                                                
+/* contoh pemakaian IF satu kasus */
+/* membaca nilai integer, menuliskan nilainya jika positif */
+
+#include <stdio.h>
+
+int main () {
+    /* Kamus */
+
+    int a;
+
+    /* Program */
+    printf("Contoh IF satu kasus \n");
+    printf("Ketikkan suatu nilai integer : ");
+    scanf("%d", &a);
+
+    if (a > 0) {
+        printf("Nilai a positif %d \n", a);
+    }
+
+    /* jika nilai a tidak positif maka program tidak melakukan apa-apa */
+    return 0;
+}
