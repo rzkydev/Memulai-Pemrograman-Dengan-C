6bec798091d6935dd836e98eed073c14dcb27872
R A U L <rzky.dev.id@gmail.com>
2025-06-11 16:35:32 +0700

    17th commit

---
+++ IF_ELSE_IF.c
@@ -0,0 +1,32 @@
+/* File: IF_ELSE_IF.c */
+/* Penulis : Muhammad Rizky Aulia Ramadhan */
+/* Deskripsi : */                                              
+/* contoh pemakaian IF tiga kasus */
+/* Membaca sebuah nilai, */
+/* menuliskan 'Nilai a positif , nilai a', jika a >0 */
+/*            'Nilai Nol , nilai a', jika a = 0  */
+/*            'Nilai a negatif , nilai a', jika a <0  */
+
+#include <stdio.h>
+
+int main () {
+
+    /* Kamus */
+    int a;
+
+    /* Program */
+    printf("Contoh IF tiga kasus \n");
+    printf("Ketikkan suatu nilai integer : ");
+    scanf("%d", &a);
+
+    if (a > 0)
+    {
+        printf("Nilai a positif %d \n", a);
+    } else if (a == 0) {
+        printf("Nilai Nol %d \n", a);
+    } else { /* a < 0 */
+        printf("Nilai a negatif %d \n", a);
+    }
+    
+    return 0;
+}
