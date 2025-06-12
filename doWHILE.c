c7b943fe2919684025c558b62b31997f7ac5e829
R A U L <rzky.dev.id@gmail.com>
2025-06-12 07:55:22 +0700

    20th commit

---
+++ doWHILE.c
@@ -0,0 +1,21 @@
+/* File: doWHILE.c */
+/* Penulis : Muhamad Rizky Aulia Ramadhan */
+/* Deskripsi : */                                       
+/* print “Saya senang” sebanyak 3 kali */
+
+#include <stdio.h>
+
+int main () {
+    /* Kamus */
+
+    int i = 0;
+
+    /* Algoritma */
+
+    do {
+        printf("Saya senang belajar bahasa C \n");
+        i++;
+    } while (i < 5);
+    
+    return 0;
+}
