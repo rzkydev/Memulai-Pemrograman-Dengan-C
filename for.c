309b6d79c37e59bac46e96fd83ec52710a80755d
R A U L <rzky.dev.id@gmail.com>
2025-06-12 07:47:11 +0700

    18th commit

---
+++ for.c
@@ -0,0 +1,18 @@
+/* File: for.c */
+/* Penulis : Muhammad Rizky Aulia Ramadhan */
+/* Deskripsi :                                                   
+/* print “Saya senang” sebanyak 3 kali */
+
+#include <stdio.h>
+
+int main () {
+    /* Kamus */
+    int i;
+
+    /* Algoritma */
+    for (i = 0; i < 3; i++) {
+        printf("Saya senang belajar bahasa C \n");
+    }
+
+    return 0;
+}
